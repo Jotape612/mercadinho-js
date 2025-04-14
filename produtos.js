fb3601616b85e7e32449dce85c1dfecf2cbb68cf
João Paulo Oliveira <158469595+Jotape612@users.noreply.github.com>
2025-04-13 21:12:56 -0300

    produtos.js

--- produtos.js
+++ produtos.js
@@ -1,18 +1,19 @@
-class Produto{
-    constructor (id, nome, preco, quantidade){
-        this.id = id;
-        this.nome = nome;
-        this.preco = preco;
-        this.quantidade = quantidade;
+class Produto {
+    constructor(id, nome, preco, quantidade) {
+      this.id = id;
+      this.nome = nome;
+      this.preco = preco;
+      this.quantidade = quantidade;
     }
-    //Não temos banco de dados então o atualizarEstoque não vai funcionar como deveria
-    atualizarEstoque(novaQuantidade){
-        this.quantidade = novaQuantidade;
+  
+    atualizarEstoque(novaQuantidade) {
+      this.quantidade = novaQuantidade;
     }
-
-    calcularValorTotal(){
-        return this.preco * this.quantidade;
+  
+    calcularValorTotal() {
+      return this.preco * this.quantidade;
     }
-}
-
-module.exports = Produto;
+  }
+  
+  module.exports = { Produto }; //Exportar um objeto que contem a classe "Produto"
+  
