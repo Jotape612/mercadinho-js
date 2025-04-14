a87a8c6c20378d268a873a2a38af7d57c6058a2d
João Paulo Oliveira <158469595+Jotape612@users.noreply.github.com>
2025-04-13 21:15:26 -0300

    venda.js
    Aqui o arquivo está com a função async e await, é extremamente funcional em uma operação de teste de integração (afinal, são varios testes juntos).

--- venda.js
+++ venda.js
@@ -16,5 +16,32 @@ class Venda {
     }
   }
   
-  module.exports = Venda;
+  const db = require('./firebase');
+  const { collection, addDoc } = require('firebase/firestore');
+  
+//Aqui a função é "async" ou seja "NÃO-SINCRONIZADA", a gente usa ela porquê o Firebase demora um pouco para salvar os dados
+
+  async function salvarVendaNoFirebase(venda) {
+    try {
+      //await, ou então "aguarde", é a confirmação do Firebase antes de continuar
+      const docRef = await addDoc(collection(db, 'vendas'), {
+        id: venda.id,
+        data: venda.data.toISOString(),
+        produtos: venda.produtos.map(item => ({
+          nome: item.produto.nome,
+          preco: item.produto.preco,
+          quantidade: item.quantidade
+        })),
+        total: venda.calcularTotal()
+      });
+  
+      return docRef.id; // Retornar doc referente.id, ou seja, retorna o ID do documento já salvado no Firebase
+    } catch (error) {
+      console.error('Erro ao salvar venda:', error);
+      throw error;
+    }
+  }
+  
+  module.exports = { Venda, salvarVendaNoFirebase };
+  
   
